26365f295109365d9fe2e9c0acb4f92eb9baa639
Marlon Peralta <marlonp721@gmail.com>
2022-01-30 10:02:16 -0500

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use App\Modules\Ir21\Entities\CronTask;
 use App\Modules\Security\Entities\Event_Logs;
 use App\Modules\Ir21\Entities\Ir21_Documents;
@@ -19,126 +20,11 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-      Validator::extend('exclude_superuser', function($attribute, $value, $parameters, $validator) {
-
-
-        if ( ! $value )
-        {
-          return true;
+        if (env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https://');
         }
-
-        $role = Role::findOrFail($value);
-
-        if ( $role->isSuperuser() )
-        {
-          return false;
-        }
-        return true;
-      });
-
-      Validator::extend('exists_value', function($attribute, $value, $parameters, $validator) {
-        if(is_null($value)){
-          return true;
-        }
-        $query = RoamingAgreement::where([[$parameters[0],$value]])->get()->count();
-
-        if($query>0){
-          return true;
-        }
-        return false;
-      });
-      Role::created(function($model){
-
-        $event_logs =new Event_Logs();
-
-        $event_logs->user_id        = auth()->user()->id;
-        $event_logs->type_log       = Event_Logs::TYPE_LOG;
-        $event_logs->element        = Event_Logs::ELEMENT_ROLE;
-        $event_logs->action         = Event_Logs::ACTION_NEW;
-        $event_logs->description    = 'Nuevo Perfil : "'.$model->display_name.'"';
-
-        try{
-          $event_logs->save();
-        }catch (\Exception $e) {
-          abort(500);
-        }
-
-
-      });
-
-      Role::deleted(function($model){
-        $event_logs =new Event_Logs();
-
-        $event_logs->user_id        = auth()->user()->id;
-        $event_logs->type_log       = Event_Logs::TYPE_LOG;
-        $event_logs->element        = Event_Logs::ELEMENT_ROLE;
-        $event_logs->action         = Event_Logs::ACTION_DELETE;
-        $event_logs->description    = 'Borrar Perfil : "'.$model->display_name.'"';
-
-        try{
-          $event_logs->save();
-        }catch (\Exception $e) {
-          abort(500);
-        }
-
-
-      });
-
-      User::created(function($model){
-
-        $event_logs =new Event_Logs();
-
-        $event_logs->user_id        = auth()->user()->id;
-        $event_logs->type_log       = Event_Logs::TYPE_LOG;
-        $event_logs->element        = Event_Logs::ELEMENT_USER;
-        $event_logs->action         = Event_Logs::ACTION_NEW;
-        $event_logs->description    = 'Nuevo Usuario : "'.$model->username.'"';
-
-        try{
-          $event_logs->save();
-        }catch (\Exception $e) {
-          abort(500);
-        }
-
-
-      });
-
-      User::updated(function($model){
-        $event_logs =new Event_Logs();
-
-        $event_logs->user_id        = auth()->user()->id;
-        $event_logs->type_log       = Event_Logs::TYPE_LOG;
-        $event_logs->element        = Event_Logs::ELEMENT_USER;
-        $event_logs->action         = Event_Logs::ACTION_EDIT;
-        $event_logs->description    = 'Editar Usuario : "'.$model->username.'"';
-
-        try{
-          $event_logs->save();
-        }catch (\Exception $e) {
-          abort(500);
-        }
-
-
-      });
-      User::deleted(function($model){
-        $event_logs =new Event_Logs();
-
-        $event_logs->user_id        = auth()->user()->id;
-        $event_logs->type_log       = Event_Logs::TYPE_LOG;
-        $event_logs->element        = Event_Logs::ELEMENT_USER;
-        $event_logs->action         = Event_Logs::ACTION_DELETE;
-        $event_logs->description    = 'Borrar Usuario : "'.$model->username.'"';
-
-        try{
-          $event_logs->save();
-        }catch (\Exception $e) {
-          abort(500);
-        }
-
-
-      });
     }
 
     /**
@@ -148,6 +34,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        if (env('REDIRECT_HTTPS')) {
+        $this->app['request']->server->set('HTTPS', true);
+        }
         // Do nothing because of X and Y.
     }
 }
